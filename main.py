0adfe8c19b4bb0348768bae2798e90d8977c7ba0
yuuto52925goal <yuuto52925goal@gmail.com>
2024-06-23 22:00:15 +0900

    fixing deploy bug

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
